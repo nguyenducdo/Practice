7ae5e8d06013ef6e3d8ccab816c5906312d75dbf
do nguyen <do-nguyen@dimage.co.jp>
2021-07-15 14:28:01 +0700

    first laravel 21-7-15

--- test.php
+++ test.php
@@ -1,3 +1,22 @@
 <?php
 // phpinfo();
-print_r(get_loaded_extensions());
+// print_r(get_loaded_extensions());
+
+class A {
+    public static $foo = 1;
+}
+
+function test ($a, ...$b) {
+    echo A::$foo . "\n";
+    echo "function: " . __FUNCTION__ . "\n";
+    var_dump($a);
+    var_dump($b);
+    subTest(...[5,6,7,8]);
+}
+
+function subTest(...$c) {
+    echo "function: " . __FUNCTION__ . "\n";
+    var_dump($c);
+}
+
+test(1,2,3,4);
